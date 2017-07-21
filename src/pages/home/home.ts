8c28f501f1dfde80a2ce206fc47762d5ea7d86bc
Ruhland Matthias <matthias.ruhland@bemondis.de>
2017-07-21 11:56:51 +0200

    Implemented BLE

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BLE } from '@ionic-native/ble';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    protected bleEnabled = false;
 
+  constructor(public navCtrl: NavController,
+                 private ble: BLE ) {
   }
+
+  protected ionViewWillEnter(): void {
+        this.ble.isEnabled().then(
+            () => {
+                this.bleEnabled = true;
+            }, () => {
+                this.bleEnabled = false;
+                this.ble.enable().then(
+                    () => {
+                        this.bleEnabled = true;
+                    }, () => {
+                        this.bleEnabled = false;
+                    });
+            });
+    }
 
 }
